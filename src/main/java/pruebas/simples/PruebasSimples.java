d607aafa6e29f96705f5514056d7541fa7c8230e
Homitowen <pycgames@gmail.com>
2022-09-26 22:44:33 -0300

    add entidad y forma de obtener la data de la tabla

--- src/main/java/pruebas/simples/PruebasSimples.java
+++ src/main/java/pruebas/simples/PruebasSimples.java
@@ -1,10 +1,32 @@
 package pruebas.simples;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 public class PruebasSimples {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-
+        Document doc = Jsoup.connect("https://listadepreciosmelar.com.ar/").get();
+        Elements cuerpo = doc.getElementsByTag("script");
+        cuerpo.forEach(
+                script -> {
+                    String resultado = script.data();
+                    if (resultado.contains("data")){
+                        System.out.println("incluye data");
+                        for (String linea: resultado.split(";")
+                             ) {
+                            if (linea.contains("var data =")){
+                                System.out.println(linea);
+                            }
+                        }
+                    }
+                }
+        );
 
 
 
---
+++ src/main/java/pruebas/simples/entidad/MelarEntidad.java
@@ -0,0 +1,13 @@
+package pruebas.simples.entidad;
+
+public class MelarEntidad {
+    private String categoria;
+    private String producto;
+    private Integer fraccion;
+    private Integer granel;
+    private String origen;
+    private String medida;
+    //son precios que no tienen en cuenta el iva
+    private Double precioFraccionado;
+    private Double precioGranel;
+}
