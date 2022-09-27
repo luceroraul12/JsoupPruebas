13dccc66ca22b37af70d41b1134340ece2c4dc8e
Lucero Raul <luceroraul12@gmail.com>
2022-09-27 12:54:52 -0300

    commit sin estructura

--- src/main/java/pruebas/simples/PruebasSimples.java
+++ src/main/java/pruebas/simples/PruebasSimples.java
@@ -12,21 +12,7 @@ public class PruebasSimples {
     public static void main(String[] args) throws IOException {
 
         Document doc = Jsoup.connect("https://listadepreciosmelar.com.ar/").get();
-        Elements cuerpo = doc.getElementsByTag("script");
-        cuerpo.forEach(
-                script -> {
-                    String resultado = script.data();
-                    if (resultado.contains("data")){
-                        System.out.println("incluye data");
-                        for (String linea: resultado.split(";")
-                             ) {
-                            if (linea.contains("var data =")){
-                                System.out.println(linea);
-                            }
-                        }
-                    }
-                }
-        );
+
 
 
 
