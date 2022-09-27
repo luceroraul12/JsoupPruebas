b3cb5661f02f90c10d4c2c6e98cdc08a462788ab
Lucero Raul <luceroraul12@gmail.com>
2022-09-27 15:05:03 -0300

    ejemplo

--- src/main/java/pruebas/simples/PruebasSimples.java
+++ src/main/java/pruebas/simples/PruebasSimples.java
@@ -1,5 +1,6 @@
 package pruebas.simples;
 
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,7 +12,32 @@ public class PruebasSimples {
 
     public static void main(String[] args) throws IOException {
 
-        Document doc = Jsoup.connect("https://listadepreciosmelar.com.ar/").get();
+        Document doc = Jsoup.connect("https://www.codetriage.com/?language=Java").get();
+
+        System.out.printf("title: %s\n", doc.title());
+
+        Elements repositoies = doc.getElementsByClass("repo-item");
+
+        for (Element repo : repositoies){
+            String repoTitle = repo.getElementsByClass("repo-item-title").text();
+            String repoIssues = repo.getElementsByClass("repo-item-issues").text();
+            String repoDescription = repo.getElementsByClass("repo-item-description").text();
+            String repoGitHubName = repo.getElementsByClass("repo-item-full-name").text();
+
+            String repoGitHubLink = "https://gitjub.com/"+ repoGitHubName.replaceAll("[()]","");
+
+            System.out.println(repoTitle + " - " + repoIssues);
+            System.out.println(repoDescription);
+            System.out.println(repoGitHubLink);
+            System.out.println();
+        }
+
+
+
+
+
+
+
 
 
 
