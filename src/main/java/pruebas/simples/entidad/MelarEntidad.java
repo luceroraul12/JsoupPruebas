9bac54ad8ae9bcaa5b89a5a952f489c69cfd30c5
Lucero Raul <luceroraul12@gmail.com>
2022-09-27 19:34:07 -0300

    entidad simplificada

--- src/main/java/pruebas/simples/entidad/MelarEntidad.java
+++ src/main/java/pruebas/simples/entidad/MelarEntidad.java
@@ -1,13 +1,41 @@
 package pruebas.simples.entidad;
 
 public class MelarEntidad {
-    private String categoria;
     private String producto;
-    private Integer fraccion;
-    private Integer granel;
-    private String origen;
-    private String medida;
     //son precios que no tienen en cuenta el iva
     private Double precioFraccionado;
     private Double precioGranel;
+
+    public String getProducto() {
+        return this.producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public Double getPrecioFraccionado() {
+        return this.precioFraccionado;
+    }
+
+    public void setPrecioFraccionado(Double precioFraccionado) {
+        this.precioFraccionado = precioFraccionado;
+    }
+
+    public Double getPrecioGranel() {
+        return this.precioGranel;
+    }
+
+    public void setPrecioGranel(Double precioGranel) {
+        this.precioGranel = precioGranel;
+    }
+
+    @Override
+    public String toString() {
+        return "MelarEntidad{" +
+                "\nproducto='" + producto + '\'' +
+                ", \nprecioFraccionado=" + precioFraccionado +
+                ", \nprecioGranel=" + precioGranel +
+                '}';
+    }
 }
