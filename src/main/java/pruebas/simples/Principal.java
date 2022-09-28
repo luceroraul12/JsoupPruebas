83973ad884beb5d86d7b7fe782f2b5a75b643b0f
Lucero Raul <luceroraul12@gmail.com>
2022-09-28 00:56:10 -0300

    la granja probada

--- src/main/java/pruebas/simples/Principal.java
+++ src/main/java/pruebas/simples/Principal.java
@@ -1,5 +1,6 @@
 package pruebas.simples;
 
+import pruebas.simples.servicios.LaGranjaDelCentroServicio;
 import pruebas.simples.servicios.SudamerikServicio;
 
 import java.io.IOException;
@@ -8,10 +9,13 @@ public class Principal {
 
     public static void main(String[] args) throws IOException {
 
-        //voy a probar con Sudamerik
-        SudamerikServicio sudamerikServicio = new SudamerikServicio();
-        sudamerikServicio.getProductosRecolectados().forEach(System.out::println);
+//        //voy a probar con Sudamerik
+//        SudamerikServicio sudamerikServicio = new SudamerikServicio();
+//        sudamerikServicio.getProductosRecolectados().forEach(System.out::println);
 
+        //voy a probar con La granja
+        LaGranjaDelCentroServicio granjaServicio = new LaGranjaDelCentroServicio();
+        granjaServicio.getProductosRecolectados().forEach(System.out::println);
 
 
     }
