b0b74e0a163c20a65c20688f5fc889621cf8f32e
Lucero Raul <luceroraul12@gmail.com>
2022-09-28 01:16:07 -0300

    lombok agregado

--- src/main/java/pruebas/simples/entidad/MelarEntidad.java
+++ src/main/java/pruebas/simples/entidad/MelarEntidad.java
@@ -1,41 +1,18 @@
 package pruebas.simples.entidad;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
 public class MelarEntidad {
     private String producto;
     //son precios que no tienen en cuenta el iva
     private Double precioFraccionado;
     private Double precioGranel;
 
-    public String getProducto() {
-        return this.producto;
-    }
-
-    public void setProducto(String producto) {
-        this.producto = producto;
-    }
-
-    public Double getPrecioFraccionado() {
-        return this.precioFraccionado;
-    }
-
-    public void setPrecioFraccionado(Double precioFraccionado) {
-        this.precioFraccionado = precioFraccionado;
-    }
-
-    public Double getPrecioGranel() {
-        return this.precioGranel;
-    }
-
-    public void setPrecioGranel(Double precioGranel) {
-        this.precioGranel = precioGranel;
-    }
-
-    @Override
-    public String toString() {
-        return "MelarEntidad{" +
-                "\nproducto='" + producto + '\'' +
-                ", \nprecioFraccionado=" + precioFraccionado +
-                ", \nprecioGranel=" + precioGranel +
-                '}';
-    }
 }
