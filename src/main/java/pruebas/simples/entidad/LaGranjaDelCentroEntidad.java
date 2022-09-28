bd3cb9ff79ad6feed9abf367254813fb18d9b592
Lucero Raul <luceroraul12@gmail.com>
2022-09-27 21:40:31 -0300

    Entidades terminadas de momento

---
+++ src/main/java/pruebas/simples/entidad/LaGranjaDelCentroEntidad.java
@@ -0,0 +1,6 @@
+package pruebas.simples.entidad;
+
+public class LaGranjaDelCentroEntidad {
+    private String nombre;
+    private Long precio;
+}
---
+++ src/main/java/pruebas/simples/entidad/SudamerikEntidad.java
@@ -0,0 +1,7 @@
+package pruebas.simples.entidad;
+
+public class SudamerikEntidad {
+    private String nombre;
+    private Integer unidades;
+    private Long precio;
+}
