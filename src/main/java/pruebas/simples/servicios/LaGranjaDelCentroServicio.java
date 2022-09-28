6901e5ac9945f91838ab31136f2d5ebe041a3728
Lucero Raul <luceroraul12@gmail.com>
2022-09-28 00:56:35 -0300

    implementacion

--- src/main/java/pruebas/simples/servicios/LaGranjaDelCentroServicio.java
+++ src/main/java/pruebas/simples/servicios/LaGranjaDelCentroServicio.java
@@ -1,4 +1,56 @@
 package pruebas.simples.servicios;
 
-public class LaGranjaDelCentroServicio {
+import org.jsoup.select.Elements;
+import pruebas.simples.entidad.LaGranjaDelCentroEntidad;
+
+public class LaGranjaDelCentroServicio extends ScrapperTablaAbstract{
+
+    public LaGranjaDelCentroServicio() {
+        setUrlBuscador("https://lagranjadelcentro.com.ar/productos.php?pagina=");
+        setClasesTabla("box-content-1");
+        setClasesNombreProducto("h3-content-1");
+        setClasesPrecio("p-precio-content-1");
+    }
+
+    @Override
+    protected void trabajarProductos(Elements productos) {
+
+        productos.forEach( producto -> {
+            String nombre = producto.getElementsByClass("h3-content-1").text();
+            String precio = producto.getElementsByClass("p-precio-content-1").text();
+
+            agregarProducto(
+                    LaGranjaDelCentroEntidad
+                            .builder()
+                            .nombreProducto(
+                                    producto.getElementsByClass("h3-content-1").text()
+                            )
+                            .precio(
+                                    Double.valueOf(producto
+                                            .getElementsByClass("p-precio-content-1")
+                                            .text()
+                                            .replaceAll("[$.]","")
+                                            .replaceAll(",","."))
+                            )
+                            .build()
+            );
+            System.out.println(
+                    LaGranjaDelCentroEntidad
+                            .builder()
+                            .nombreProducto(
+                                    producto.getElementsByClass("h3-content-1").text()
+                            )
+                            .precio(
+                                    Double.valueOf(producto
+                                            .getElementsByClass("p-precio-content-1")
+                                            .text()
+                                            .replaceAll("[$.]","")
+                                            .replaceAll(",","."))
+                            )
+                            .build()
+            );
+        });
+
+
+    }
 }
