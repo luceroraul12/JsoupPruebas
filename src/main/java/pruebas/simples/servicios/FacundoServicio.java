a7af48bb7451c1c3cc63398d3fabdc742d05c801
Lucero Raul <luceroraul12@gmail.com>
2022-10-15 10:39:07 -0300

    Facundo Distribuidora queda en un stop hasta saber si pueden pasar el excel y no el pdf que contiene la tabla de excel

---
+++ src/main/java/pruebas/simples/servicios/FacundoServicio.java
@@ -0,0 +1,42 @@
+package pruebas.simples.servicios;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+import org.fit.pdfdom.PDFDomTree;
+import org.fit.pdfdom.PDFToHTML;
+import org.jsoup.Jsoup;
+import org.w3c.dom.Document;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FacundoServicio {
+
+    public void mostrarContenido() throws IOException {
+        File file = new File("src/main/resources/facundo.pdf");
+        PDDocument doc = PDDocument.load(file);
+
+        PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+        stripper.setSortByPosition(true);
+
+        PDFTextStripper tStripper = new PDFTextStripper();
+
+        String pdfFileInText = tStripper.getText(doc);
+
+        System.out.println(pdfFileInText);
+
+
+
+//
+//        Document doc = Jsoup.parse(file,);
+//        System.out.println(doc);
+
+
+
+
+
+    }
+
+
+}
